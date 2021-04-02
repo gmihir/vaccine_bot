7a8552a61e1d7e9baefd1e04763d07b42134c438
gmihir <m08.in.us@gmail.com>
2021-04-01 17:39:27 -0700

    DEBUG COMMIT: testing scheduling run

--- vaccine_bot.py
+++ vaccine_bot.py
@@ -59,7 +59,12 @@ def sendTweet():
         auth = tweepy.OAuthHandler(api_key, api_key_secret)
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
-        tweet_text = formTweetText(timestamp)
+
+        if(found_vaccine):
+                tweet_text = formTweetText(timestamp)
+        else:
+                tweet_text = "No appointments found as of: " + timestamp.ctime()
+        # tweet_text = formTweetText(timestamp)
         api.update_status(tweet_text)
         print("Tweet sent at: " + datetime.now().ctime())
 
@@ -68,5 +73,6 @@ checkVaccineAppointment()
 if(found_vaccine):
         sendTweet()
 else:
+        sendTweet() ## for debugging heroku and cron
         print("No vaccines available at: " + datetime.now().ctime())
 
