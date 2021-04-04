75f91a368a14b2c04c4875eec06386483c67310e
gmihir <m08.in.us@gmail.com>
2021-04-03 21:16:53 -0700

    added support for vons pharmacies

--- vaccine_bot.py
+++ vaccine_bot.py
@@ -5,6 +5,8 @@ import os
 import dateutil.parser
 from datetime import datetime
 import re
+from pytz import timezone
+import pytz
 
 
 state = "CA"
@@ -17,21 +19,56 @@ bearer_token = os.environ.get("BEARER_TOKEN")
 access_token = os.environ.get("ACCESS_TOKEN")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
-found_vaccine = False
-timestamp = datetime.now()
+
+#REQUEST INFO
+
+## cvs
+cvs_request_url = os.environ.get("CVS_REQUEST_URL")
+
+## walgreens
+walgreens_request_url = os.environ.get("WALGREENS_REQUEST_URL")
+walgreens_cookie = os.environ.get("WALGREENS_COOKIE")
+walgreens_token = os.environ.get("WALGREENS_TOKEN")
+
+## vons
+vons_request_url = os.environ.get("VONS_REQUEST_URL")
+
+#RESERVATION/VENDOR INFO
+
+## cvs
+cvs_reservation_url = "https://www.cvs.com/immunizations/covid-19-vaccine"
+cvs_vendor = "CVS"
+
+## walgreens
+walgreens_reservation_url = "https://www.walgreens.com/findcare/vaccination/covid-19"
+walgreens_vendor = "Walgreens"
+
+## vons
+vons_reservation_url = "https://www.mhealthappointments.com/covidappt"
+vons_vendor = "Vons"
 
 
-def formCVSURL():
-        return("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status." + state + ".json?vaccineinfo")
+longitude = os.environ.get("LONGITUDE")
+latitude = os.environ.get("LATITUDE")
 
-def checkVaccineAppointment():
+
+found_vaccine_cvs = False
+found_vaccine_vons = False
+found_vaccine_walgreens = False
+
+timestamp = datetime.now(tz=pytz.utc)
+timestamp = timestamp.astimezone(timezone('US/Pacific'))
+
+current_time = datetime.now(tz=pytz.utc)
+current_time = current_time.astimezone(timezone('US/Pacific'))
+
+def checkCVSVaccineAppointment():
         global timestamp
-        global found_vaccine
-        # form request url
-        url = formCVSURL()
+        global found_vaccine_cvs
+        
 
         # get response and convert to json 
-        response = requests.get(url, headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
+        response = requests.get(cvs_request_url, headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
         vaccine_availability = response.json()
         # print(vaccine_availability)
         timestamp = dateutil.parser.parse(vaccine_availability["responsePayloadData"]["currentTime"])
@@ -40,36 +77,63 @@ def checkVaccineAppointment():
         for city_json in cities_json:
                 if(city_json["city"] == city):
                         # print(city_json["status"])
-                        found_vaccine = (city_json["status"] == "Available")
+                        found_vaccine_cvs = (city_json["status"] == "Available")
                         # done looking for city, can exit loop now
                         break
 
 
+def checkVonsVaccineAppointment():
+        vons_response = requests.get(vons_request_url)
+        vons_vaccine_availability = vons_response.json()
+        if(vons_vaccine_availability):
+                vons_vaccine_availability = True
 
-def formTweetText(updated_timestamp):
+
+
+def formTweetText(vendor, updated_timestamp, reservation_url):
         time_string = updated_timestamp.ctime()
 
         ## strip out extra spaces
         time_string = "As of: " + re.sub('\s+', ' ', time_string)
-        notification_string = "Appointments found at CVS locations in San Diego!"
+        notification_string = "Appointments found at " + vendor + " locations in San Diego!"
         reservation_string = "Schedule here:" 
-        reservation_url = "https://www.cvs.com/immunizations/covid-19-vaccine"
         return (time_string + "\n\n" + notification_string + "\n\n" + reservation_string + "\n" + reservation_url)
         
-def sendTweet():
+def sendTweet(text):
         auth = tweepy.OAuthHandler(api_key, api_key_secret)
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
 
 
-        tweet_text = formTweetText(timestamp)
-        api.update_status(tweet_text)
-        print("Tweet sent at: " + datetime.now().ctime())
+        api.update_status(text)
+        print("Tweet sent at: " + current_time.ctime())
+        print("Tweet text: " + text)
 
-checkVaccineAppointment()
 
-if(found_vaccine):
-        sendTweet()
-else:
-        print("No vaccines available at: " + datetime.now().ctime())
+# check cvs
+try:
+        checkCVSVaccineAppointment()
+        if(found_vaccine_cvs):
+                cvs_tweet_text = formTweetText(cvs_vendor, timestamp, cvs_reservation_url)
+                sendTweet(cvs_tweet_text)
+        else:
+                print("No CVS vaccines available at: " + datetime.now().ctime())
+except:
+        # something failed, log failure
+        print("EXCEPTION IN FINDING CVS VACCINES")
+
+## check vons
+try:
+        checkVonsVaccineAppointment()
+        if(found_vaccine_vons):
+                vons_tweet_text = formTweetText(vons_vendor, current_time, vons_reservation_url)
+                sendTweet(vons_tweet_text)
+        else:
+                print("No Vons vaccines available at: " + current_time.ctime())
+except:
+        print("EXCEPTION IN FINDING VONS VACCINES")
+
+
+
+
 
